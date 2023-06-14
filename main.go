fa1e31ceb27a3a752fe725ff17698af0b26a547e
Matthew Setter <matthew@matthewsetter.com>
2023-06-14 10:44:31 +0200

    Reduce filesystem usage
    After an external review of the code, it was determined that there was
    too much usage of the filesystem, which would slow down the code. Given
    that, the code was refactored to move manipulation of files to memory.

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -11,7 +12,6 @@ import (
 	"log"
 	"mime/multipart"
 	"net/http"
-	"os"
 	"strconv"
 
 	"github.com/nfnt/resize"
@@ -28,64 +28,52 @@ type simpleQRCode struct {
 	Size    int
 }
 
+// Generate generates a QR code using the value of simpleQRCode.Content
 func (code *simpleQRCode) Generate() ([]byte, error) {
-	png, err := qrcode.Encode(code.Content, qrcode.Medium, code.Size)
+	qrCode, err := qrcode.Encode(code.Content, qrcode.Medium, code.Size)
 	if err != nil {
 		return nil, fmt.Errorf("could not generate a QR code: %v", err)
 	}
-	return png, nil
+	return qrCode, nil
 }
 
-func (code *simpleQRCode) GenerateWithWatermark() ([]byte, error) {
-	err := qrcode.WriteFile(code.Content, qrcode.Medium, code.Size, QRCODE_FILENAME)
+// GenerateWithWatermark generates a QR code using the value of simpleQRCode.Content
+// and adds a watermark to it, centered in the middle of the QR code, using the
+// supplied watermark image data
+func (code *simpleQRCode) GenerateWithWatermark(watermark []byte) ([]byte, error) {
+	qrCode, err := code.Generate()
 	if err != nil {
 		return nil, err
 	}
 
-	err = code.addWatermark(code.Size)
+	qrCode, err = code.addWatermark(qrCode, watermark, code.Size)
 	if err != nil {
 		return nil, fmt.Errorf("could not add watermark to QR code: %v", err)
 	}
 
-	fileData, err := os.ReadFile(QRCODE_FILENAME)
-	if err != nil {
-		return nil, fmt.Errorf("could not open watermarked QR code: %v", err)
-	}
-
-	return fileData, nil
+	return qrCode, nil
 }
 
-func (code *simpleQRCode) addWatermark(size int) error {
-	qrCode, err := os.Open(QRCODE_FILENAME)
+// addWatermark adds a watermark to a QR code, centered in the middle of the QR code
+func (code *simpleQRCode) addWatermark(qrCode []byte, watermarkData []byte, size int) ([]byte, error) {
+	qrCodeData, err := png.Decode(bytes.NewBuffer(qrCode))
 	if err != nil {
-		return fmt.Errorf("could not open QR code file: %v", err)
-	}
-	defer qrCode.Close()
-
-	originalImage, err := png.Decode(qrCode)
-	if err != nil {
-		return fmt.Errorf("could not decode QR code file: %v", err)
+		return nil, fmt.Errorf("could not decode QR code: %v", err)
 	}
 
-	watermark, err := os.Open(WATERMARK_FILENAME)
+	watermarkImage, err := png.Decode(bytes.NewBuffer(watermarkData))
 	if err != nil {
-		return fmt.Errorf("could not open watermark image: %v", err)
-	}
-	defer watermark.Close()
-
-	watermarkImage, err := png.Decode(watermark)
-	if err != nil {
-		return fmt.Errorf("could not decode watermark image: %v", err)
+		return nil, fmt.Errorf("could not decode watermark: %v", err)
 	}
 
-	// Center the watermark on the QR code
+	// Determine the offset to center the watermark on the QR code
 	offset := image.Pt(((size / 2) - 32), ((size / 2) - 32))
 
-	// Use same size as source image has
-	b := originalImage.Bounds()
-	m := image.NewRGBA(b)
+	watermarkImageBounds := qrCodeData.Bounds()
+	m := image.NewRGBA(watermarkImageBounds)
 
-	draw.Draw(m, b, originalImage, image.Point{}, draw.Src)
+	// Center the watermark over the QR code
+	draw.Draw(m, watermarkImageBounds, qrCodeData, image.Point{}, draw.Src)
 	draw.Draw(
 		m,
 		watermarkImage.Bounds().Add(offset),
@@ -94,72 +82,41 @@ func (code *simpleQRCode) addWatermark(size int) error {
 		draw.Over,
 	)
 
-	out, err := os.Create(QRCODE_FILENAME)
-	if err != nil {
-		return fmt.Errorf("could not create watermarked QR code: %v", err)
-	}
-	defer out.Close()
+	watermarkedQRCode := bytes.NewBuffer(nil)
+	png.Encode(watermarkedQRCode, m)
 
-	png.Encode(out, m)
-
-	return nil
+	return watermarkedQRCode.Bytes(), nil
 }
 
-func uploadWatermarkImage(file multipart.File) error {
-	dst, err := os.Create(WATERMARK_FILENAME)
+// resizeWatermark resizes a watermark image to the desired width and height
+func resizeWatermark(watermark io.Reader, width uint) ([]byte, error) {
+	decodedImage, err := png.Decode(watermark)
 	if err != nil {
-		return fmt.Errorf("could not upload the watermark file: %v", err)
+		return nil, fmt.Errorf("could not decode watermark image: %v", err)
 	}
-	defer dst.Close()
-
-	if _, err := io.Copy(dst, file); err != nil {
-		return fmt.Errorf("could not upload the watermark file: %v", err)
-	}
-
-	return nil
-}
-
-func resizeWatermarkImage(width uint) error {
-	image, err := os.Open(WATERMARK_FILENAME)
-	if err != nil {
-		return fmt.Errorf("could not open watermark image: %v", err)
-	}
-
-	decodedImage, err := png.Decode(image)
-	if err != nil {
-		return fmt.Errorf("could not decode watermark image: %v", err)
-	}
-	image.Close()
 
 	m := resize.Resize(width, 0, decodedImage, resize.Lanczos3)
-	resizedImage, err := os.Create(WATERMARK_FILENAME)
-	if err != nil {
-		return fmt.Errorf("could not resize watermark image: %v", err)
-	}
-	defer resizedImage.Close()
+	resized := bytes.NewBuffer(nil)
+	png.Encode(resized, m)
 
-	png.Encode(resizedImage, m)
-
-	return nil
+	return resized.Bytes(), nil
 }
 
-func detectContentType(filename string) (string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return "", fmt.Errorf("could not open file: %v", err)
+// uploadWatermarkImage uploads an image file to be used as a watermark for a QR code
+func uploadWatermarkImage(file multipart.File) ([]byte, error) {
+	buf := bytes.NewBuffer(nil)
+	if _, err := io.Copy(buf, file); err != nil {
+		return nil, fmt.Errorf("could not copy the watermark file into memory: %v", err)
 	}
 
-	fileHeader := make([]byte, 512)
-	if _, err := file.Read(fileHeader); err != nil {
-		return "", fmt.Errorf("could not read file: %v", err)
-	}
-
-	return http.DetectContentType(fileHeader), nil
+	return buf.Bytes(), nil
 }
 
+// createErrorResponse is a small utility function to simplify returning a JSON response
+// to be returned to the user when an error has occurred
 func createErrorResponse(message string) []byte {
 	resp := make(map[string]string)
-	resp["error"] = "Only PNG files are supported."
+	resp["error"] = message
 	jsonResp, err := json.Marshal(resp)
 
 	if err != nil {
@@ -189,7 +146,7 @@ func handleRequest(writer http.ResponseWriter, request *http.Request) {
 
 	qrCode := simpleQRCode{Content: url, Size: qrCodeSize}
 
-	watermark, _, err := request.FormFile("watermark")
+	watermarkFile, _, err := request.FormFile("watermark")
 	if err != nil && errors.Is(err, http.ErrMissingFile) {
 		fmt.Println("Error retrieving the uploaded watermark image or no watermark image was uploaded. Error details: ", err)
 		codeData, err := qrCode.Generate()
@@ -203,14 +160,14 @@ func handleRequest(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	err = uploadWatermarkImage(watermark)
+	watermark, err := uploadWatermarkImage(watermarkFile)
 	if err != nil {
-		writer.Write(createErrorResponse("Could not generate QR code with the provided watermark image."))
+		writer.Write(createErrorResponse("Could not upload the watermark image."))
 		writer.WriteHeader(400)
 		return
 	}
 
-	contentType, err := detectContentType(WATERMARK_FILENAME)
+	contentType := http.DetectContentType(watermark)
 	if err != nil {
 		writer.Write(createErrorResponse(
 			fmt.Sprintf("Provided watermark image is not a PNG image. %v. Content type is: %s", err, contentType)),
@@ -219,15 +176,22 @@ func handleRequest(writer http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	resizeWatermarkImage(WATERMARK_WIDTH)
-
-	defer watermark.Close()
-	fileData, err := qrCode.GenerateWithWatermark()
+	watermark, err = resizeWatermark(bytes.NewBuffer(watermark), WATERMARK_WIDTH)
 	if err != nil {
-		writer.Write(createErrorResponse("Could not generate QR code with the provided watermark image."))
+		writer.Write(createErrorResponse("Could not resize the watermark image."))
 		writer.WriteHeader(400)
 		return
 	}
+
+	fileData, err := qrCode.GenerateWithWatermark(watermark)
+	if err != nil {
+		writer.Write(
+			createErrorResponse(
+				fmt.Sprintf("Could not generate QR code with the provided watermark image: %v", err)))
+		writer.WriteHeader(400)
+		return
+	}
+
 	writer.Write(fileData)
 }
 
